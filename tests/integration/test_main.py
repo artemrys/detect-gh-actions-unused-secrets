724d019078aa16107a5f150260bd94d42a7c099d
Artem Rys <rysartem@gmail.com>
2022-07-06 14:28:56 +0200

    tests: move tests to another repo

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -4,17 +4,17 @@ from detect_gh_actions_unused_secrets import main
 
 
 def test_main(capsys):
-    expected_output = """artemrys/test-repo
+    expected_output = """artemrys/detect-gh-actions-unused-secrets
 \tSecrets ['UNUSED_SECRET_1', 'UNUSED_SECRET_2', 'USED_SECRET_KEY']
 \tGithub Actions file .github/workflows/main.yml
 \tUNUSED_SECRET_1 is not used anywhere!
 \tUNUSED_SECRET_2 is not used anywhere!
-\tUSED_SECRET_KEY is used in .github/workflows/main.yml, line 14
+\tUSED_SECRET_KEY is used in .github/workflows/main.yaml, line 18
 """
     main(
         (
             os.getenv("GH_TOKEN_INTEGRATION_TEST"),
-            "artemrys/test-repo",
+            "artemrys/detect-gh-actions-unused-secrets",
         )
     )
     captured = capsys.readouterr()
