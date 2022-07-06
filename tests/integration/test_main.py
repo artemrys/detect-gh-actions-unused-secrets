7409f1950052a4c07635de7831a6380ea6aee7b4
Artem Rys <rysartem@gmail.com>
2022-07-06 14:48:12 +0200

    chore: typo

--- tests/integration/test_main.py
+++ tests/integration/test_main.py
@@ -6,7 +6,7 @@ from detect_gh_actions_unused_secrets import main
 def test_main(capsys):
     expected_output = """artemrys/detect-gh-actions-unused-secrets-demo
 \tSecrets ['UNUSED_SECRET_1', 'UNUSED_SECRET_2', 'USED_SECRET_KEY']
-\tGithub Actions file .github/workflows/main.yml
+\tGithub Actions file .github/workflows/main.yaml
 \tUNUSED_SECRET_1 is not used anywhere!
 \tUNUSED_SECRET_2 is not used anywhere!
 \tUSED_SECRET_KEY is used in .github/workflows/main.yaml, line 18
